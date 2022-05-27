94f54f3582e010b6456bda446c84524442c85116
DmitryGaydabura <gaydabura.d@icloud.com>
2022-05-27 23:54:52 +0300

    Initial commit

---
+++ src/Human.java
@@ -0,0 +1,14 @@
+public class Human {
+    String name;
+    boolean isMale;
+    String nameOfMother;
+    String nameOfFather;
+
+
+    public Human(String name, boolean isMale, String nameOfMother, String nameOfFather) {
+        this.name = name;
+        this.isMale = isMale;
+        this.nameOfMother = nameOfMother;
+        this.nameOfFather = nameOfFather;
+    }
+}
